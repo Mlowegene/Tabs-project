9f22193735b487dbe9f6c288e3182b583c840ec3
Mlowegene <genemlowe@gmail.com>
2022-04-13 14:25:11 +0300

    i put css in button

--- src/App.js
+++ src/App.js
@@ -39,7 +39,8 @@ const {company, dates, duties, title} = jobs[value];
         {jobs.map((item, index) => {
           return (
             <button key={item.id} onClick={() =>
-            setValue(index)}>
+            setValue(index)} 
+            className={`job-btn ${index === value && 'active-btn'}`}>
               {item.company}
             </button>
           );
